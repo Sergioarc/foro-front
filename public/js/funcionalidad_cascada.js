3a997011ea2eb7073e7f27ec7e5def4f6553eaec
Sergio <cheko@MacBook-Pro-de-Sergio.local>
2018-05-17 12:53:19 -0500

    se agrego la leyenda de las calificaciones

--- public/js/funcionalidad_cascada.js
+++ public/js/funcionalidad_cascada.js
@@ -135,3 +135,150 @@ $(function () {
     console.log("ASPECTOS ETNICOS: ", $('input[name=aspectos]:checked').val());
   });
 })
+
+function cambio(obj){
+  switch (obj.name) {
+    case 'calidad':
+      switch (obj.value) {
+        case 'calidad1':
+          $("#textCalidad").text("Valores de 1 a 4");
+          break;
+        case 'calidad2':
+          $("#textCalidad").text("Valores de 5 a 8");
+          break;
+        case 'calidad3':
+          $("#textCalidad").text("Valores de 9 a 12");
+          break;
+        case 'calidad4':
+          $("#textCalidad").text("Valores de 13 a 16");
+          break;
+        case 'calidad5':
+          $("#textCalidad").text("Valores de 17 a 20");
+          break;
+        default:
+      }
+      break;
+    case 'originalidad':
+      switch (obj.value) {
+        case 'originalidad1':
+          $("#textOriginalidad").text("Valores de 1 a 4");
+          break;
+        case 'originalidad2':
+          $("#textOriginalidad").text("Valores de 5 a 8");
+          break;
+        case 'originalidad3':
+          $("#textOriginalidad").text("Valores de 9 a 12");
+          break;
+        case 'originalidad4':
+          $("#textOriginalidad").text("Valores de 13 a 16");
+          break;
+        case 'originalidad5':
+          $("#textOriginalidad").text("Valores de 17 a 20");
+          break;
+        default:
+      }
+      break;
+    case 'cuantitativas':
+      switch (obj.value) {
+        case 'cuantitativas1':
+          $("#textCuantitativas").text("Valores de 1 a 4");
+          break;
+        case 'cuantitativas2':
+          $("#textCuantitativas").text("Valores de 5 a 8");
+          break;
+        case 'cuantitativas3':
+          $("#textCuantitativas").text("Valores de 9 a 12");
+          break;
+        case 'cuantitativas4':
+          $("#textCuantitativas").text("Valores de 13 a 16");
+          break;
+        case 'cuantitativas5':
+          $("#textCuantitativas").text("Valores de 17 a 20");
+          break;
+        default:
+      }
+      break;
+    case 'cualitativas':
+      switch (obj.value) {
+        case 'cualitativas1':
+          $("#textCualitativas").text("Valores de 1 a 4");
+          break;
+        case 'cualitativas2':
+          $("#textCualitativas").text("Valores de 5 a 8");
+          break;
+        case 'cualitativas3':
+          $("#textCualitativas").text("Valores de 9 a 12");
+          break;
+        case 'cualitativas4':
+          $("#textCualitativas").text("Valores de 13 a 16");
+          break;
+        case 'cualitativas5':
+          $("#textCualitativas").text("Valores de 17 a 20");
+          break;
+        default:
+      }
+      break;
+    case 'mixtas':
+      switch (obj.value) {
+        case 'mixtas1':
+          $("#textMixtas").text("Valores de 1 a 4");
+          break;
+        case 'mixtas2':
+          $("#textMixtas").text("Valores de 5 a 8");
+          break;
+        case 'mixtas3':
+          $("#textMixtas").text("Valores de 9 a 12");
+          break;
+        case 'mixtas4':
+          $("#textMixtas").text("Valores de 13 a 16");
+          break;
+        case 'mixtas5':
+          $("#textMixtas").text("Valores de 17 a 20");
+          break;
+        default:
+      }
+      break;
+    case 'trascendencia':
+      switch (obj.value) {
+        case 'trascendencia1':
+          $("#textTrascendencia").text("Valores de 1 a 4");
+          break;
+        case 'trascendencia2':
+          $("#textTrascendencia").text("Valores de 5 a 8");
+          break;
+        case 'trascendencia3':
+          $("#textTrascendencia").text("Valores de 9 a 12");
+          break;
+        case 'trascendencia4':
+          $("#textTrascendencia").text("Valores de 13 a 16");
+          break;
+        case 'trascendencia5':
+          $("#textTrascendencia").text("Valores de 17 a 20");
+          break;
+        default:
+      }
+      break;
+    case 'aspectos':
+      switch (obj.value) {
+        case 'aspectos1':
+          $("#textAspectos").text("Valores de 1 a 4");
+          break;
+        case 'aspectos2':
+          $("#textAspectos").text("Valores de 5 a 8");
+          break;
+        case 'aspectos3':
+          $("#textAspectos").text("Valores de 9 a 12");
+          break;
+        case 'aspectos4':
+          $("#textAspectos").text("Valores de 13 a 16");
+          break;
+        case 'aspectos5':
+          $("#textAspectos").text("Valores de 17 a 20");
+          break;
+        default:
+      }
+      break;
+    default:
+
+  }
+}
